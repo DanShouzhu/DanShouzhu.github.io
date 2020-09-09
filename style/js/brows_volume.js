49cc020ada357b0fb55b0737d4e37d49fc4c7b41
Shouzhu-Dan <z5243945@ad.unsw.edu.au>
2020-09-10 08:51:17 +1000

    brows volume

---
+++ style/js/brows_volume.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e8bf5e55e5c7d8556eb3ec6011ee1c95";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
